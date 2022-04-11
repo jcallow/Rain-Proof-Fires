801dc12d06f26b3434a6c88638547bcf4d536861
John Callow <john.j.callow@gmail.com>
2022-04-11 19:04:37 -0400

    adding mod files

---
+++ RainProofFires.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+public class RainProofFires : Mod
+{
+    private static Harmony m_harmony;
+    private const string ModName = "RainProofFires";
+    private const string HarmonyId = "com.jcallow.projects.rainprooffires";
+    public void Start()
+    {
+        m_harmony = new Harmony(HarmonyId);
+        var assembly = Assembly.GetExecutingAssembly();
+        m_harmony.PatchAll(assembly);
+        Debug.Log("Mod RainProofFires has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        m_harmony.UnpatchAll(HarmonyId);
+        Debug.Log("Mod RainProofFires has been unloaded!");
+    }
+    
+}
+
+[HarmonyPatch(typeof(Firecamp))]
+class RainProofFirePatch {
+    private static Boolean raining = false;
+    [HarmonyPrefix]
+    [HarmonyPatch("CheckRain")]
+    static bool CheckRain() {
+        return false;
+    }
+}
---
+++ build.bat
@@ -0,0 +1,2 @@
+powershell Compress-Archive RainProofFires.cs,icon.png,modinfo.json,banner.jpg modinstaller.rain-proof-fires.zip -Force
+rename modinstaller.rain-proof-fires.zip modinstaller.rain-proof-fires.ghmod
